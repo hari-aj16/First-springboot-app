e6f5e904f26d1794018821c5b458645287f9f08b
hari-aj16 <hari160107@gmail.com>
2026-06-11 12:37:24 +0530

    Delete src/main/java/com/example/HelloWorld/main.java

--- src/main/java/com/example/HelloWorld/main.java
+++
@@ -1,8 +0,0 @@
-package com.example.HelloWorld;
-
-public class main {
-    public static void main(String[] args) {
-        TodoService todoService = new TodoService();
-        todoService.printTodos();
-    }
-}
